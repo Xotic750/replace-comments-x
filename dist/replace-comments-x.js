6579d2717a3a3b55145aaea2bf406266cb414c6b
Graham Fairweather <xotic750@gmail.com>
2019-07-27 02:31:14 +0200

    :bookmark: v3.0.23
    Auto build

--- dist/replace-comments-x.js
+++ dist/replace-comments-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-26T12:51:37.461Z",
+  "date": "2019-07-27T00:31:08.940Z",
   "describe": "",
   "description": "Replace the comments in a string.",
   "file": "replace-comments-x.js",
   "hash": "23020e5dfba4cf4d3444",
   "license": "MIT",
-  "version": "3.0.22"
+  "version": "3.0.23"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
