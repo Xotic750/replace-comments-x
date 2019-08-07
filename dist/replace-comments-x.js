679d405eb1e028e65911cfe18624d6182b349760
Graham Fairweather <xotic750@gmail.com>
2019-08-07 23:54:16 +0200

    Lodash security fix

--- dist/replace-comments-x.js
+++ dist/replace-comments-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-07T01:03:55.220Z",
+  "date": "2019-08-07T21:54:11.009Z",
   "describe": "",
   "description": "Replace the comments in a string.",
   "file": "replace-comments-x.js",
