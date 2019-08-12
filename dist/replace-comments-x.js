72def15cb289dbdf20c69a22c96d4b8bf0413a7a
Graham Fairweather <xotic750@gmail.com>
2019-08-12 23:53:49 +0200

    :bookmark: v3.0.25
    Auto run

--- dist/replace-comments-x.js
+++ dist/replace-comments-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-07T21:54:11.009Z",
+  "date": "2019-08-12T21:53:44.120Z",
   "describe": "",
   "description": "Replace the comments in a string.",
   "file": "replace-comments-x.js",
